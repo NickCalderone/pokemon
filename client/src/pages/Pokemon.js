00c1c7f40d809ba31fc13e44b222be3b034e297b
NickCalderone <calderonenick@gmail.com>
2023-04-27 22:18:46 -0600

    formatted pokemon height

--- client/src/pages/Pokemon.js
+++ client/src/pages/Pokemon.js
@@ -19,19 +19,34 @@ export default function Pokemon()
 	let speciesUrl = `/api/pokemon-species/${pokemon}`;
 	let speciesData = useFetch(speciesUrl, setPokemonSpeciesLoading, {});
 
-	// console.log(data)
+	function findFlavorText( arra ){
+
+		//find the first flavor text in english
+		let myText = arra.find(element => element.language.name == "en")
+		console.log(myText.flavor_text.normalize("NFD").replace("\\u000c", " "))
+		
+		//if there is no english flavor text, return the first entry
+		return myText ? myText.flavor_text.normalize().replace("\\u000c", " ") : arra[0].flavor_text.normalize();
+	}
+
+	function getHeight(initial){
+		let inchesFloat = initial * 3.9370;
+		let feetFloat = inchesFloat / 12;
+		let feet = Math.floor(feetFloat);
+		let inches = Math.round((feetFloat - feet) * 12);
+		return `${feet}' ${inches}"`;
+	}
 
 	return (
 		<div>
-			{/* Optional chaining prevents errors when the data has yet to be loaded */}
-			{/* {data?.name} */}
-			{/* only evals first, since it doesnt need to get to second */}
 			{(pokemonLoading || pokemonSpeciesLoading) ? (<div>loading</div>) : (
 				<div>
 					<h2>{data.name}</h2>
 					<img src={data.sprites.other["official-artwork"].front_default} alt={`official image of ${data.name}`} height="475" width="475" />
-					<Types types={data.types} />
-					{speciesData.flavor_text_entries[0].flavor_text}
+					<div><Types types={data.types} /></div>
+					<p>{findFlavorText(speciesData.flavor_text_entries)}</p>
+					<p>height:</p>
+					<p>{getHeight(data.height)}</p>
 				</div>
 			)}
 		</div>
@@ -39,7 +54,8 @@ export default function Pokemon()
 }
 
 
-// null coalesing ?.
+// null coalesing 
+// optional chaining ?.
 // prevent content from eval
 // check out react query to inform how to use useFetch within react
 // bring loading state into useFetch
