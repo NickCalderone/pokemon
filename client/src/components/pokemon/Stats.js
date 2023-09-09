e28c1d7b1b0b02a2011159994e9cae712ce60b09
NickCalderone <calderonenick@gmail.com>
2023-09-09 17:53:53 -0600

    added index to props

--- client/src/components/pokemon/Stats.js
+++ client/src/components/pokemon/Stats.js
@@ -3,7 +3,7 @@ import Stat from "./Stat";
 export default function Stats({data}){
 	return (
 		data.map((item, index) => (
-			<Stat name={item.stat.name} stat={item.base_stat} key={index} />
+			<Stat name={item.stat.name} stat={item.base_stat} key={index} index={index} />
 		))
 	)
 }
